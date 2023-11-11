c7aa9f80a8d8a5686d8e1ef717e5d1250e7f9e29
Shivam Kesarwani <shivamkesarwani001@gmail.com>
2023-11-11 21:10:12 +0530

    Tooltip renamed, comments added, reset minute to 0 when minute=100

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
 const play=document.getElementById('start')
+const playCloud=document.getElementById('playCloud')
 const reset=document.getElementById('reset')
+const resetCloud=document.getElementById('resetCloud')
 const minute=document.getElementById('minute')
 const second=document.getElementById('second')
 const arm=document.getElementById('arm')
 var id, secCount, prevSecCount,minCount, prevMinCount, playButtonStatus;
 
 
-//IDEAS 
-//1. TELL HOW MANY HOURS PASSED SINCE START OF THE STOPWATCH
-//2. A TRIANGLE ICON IN PLACE OF A START BUTTON <i class="fa-solid fa-play"></i> <i class="fa-solid fa-play-pause"></i>-done
-//3. A PAUSE ICON(||) IN PLACE OF STOP BUTTON <i class="fa-solid fa-pause"></i>-done
-//4. A REFRESH ICON IN PLACE OF RESET BUTTON <i class="fa-solid fa-arrows-rotate"></i>-done
-//5. A HOVER WHICH TELLS US ABOUT THE BUTTON-done
-//6. SOLVE THE ANOMOLY ENCOUNTERED WHEN START BUTTON IS CLICKED WHEN THE STOPWATCH IS RUNNING-done
-
-
-
+//IIFE
 (function(){
     prevMinCount=0;
     prevSecCount=0;
@@ -25,11 +18,33 @@ var id, secCount, prevSecCount,minCount, prevMinCount, playButtonStatus;
     arm.style.position='absolute';
     arm.style.top='0%';
     arm.style.left='50%';
+
+
+    //adjust the size of i tags inside start/stop and reset buttons according to windows height
+    let windowHeight=window.innerHeight
+    if(windowHeight<400){
+        reset.children[0].classList.add('fa-2xs')
+        play.children[0].classList.add('fa-2xs')
+    }
+    else if(windowHeight>=400 && windowHeight<800){
+        reset.children[0].classList.add('fa-xs')
+        play.children[0].classList.add('fa-xs')
+    }
+    else if(windowHeight>=800 && windowHeight<1100){
+        
+        reset.children[0].classList.add('fa-lg')
+        play.children[0].classList.add('fa-lg')
+    }
+    else if(windowHeight>=1100){
+        reset.children[0].classList.add('fa-2xl')
+        play.children[0].classList.add('fa-2xl')
+    }
 })()
 
 
 play.addEventListener('click',function(req, res){
-
+    
+    //logic to start the stopwatch
     if(playButtonStatus=='play'){
         console.log(`Start Clicked, prevMin: ${prevMinCount}, prevSec: ${prevSecCount}`)
         secCount=prevSecCount;
@@ -37,22 +52,32 @@ play.addEventListener('click',function(req, res){
         let min, sec;
 
 
-        play.innerHTML=
-        `<i class="fa-solid fa-pause"></i>
-        <div class="cloud">Pause</div>`
+        play.innerHTML=`<i class="fa-solid fa-pause"></i>`
+        playCloud.textContent='Stop'
+
         playButtonStatus='pause'
 
+        //setInterval to increment secCount every second and increment minCount every 60 seconds
         id=setInterval(function(){
             secCount++;
+
+            //increment minCount every 60 seconds
             if(secCount==60){
                 secCount=0;
                 minCount++;
             }
 
-            console.log(`sec: ${secCount}, angle: ${angle}, prevangle: ${prevAngle}`)
+            //if minCount=100 reset the stopwatch
+            if(minCount==100){
+                clearInterval(id);
+                secCount=0;
+                minCount=0;
+            }
+
+            //rotate the arm
             let angleStr=angle+'deg'
             arm.style.transform=`rotate(${angleStr})`
-            console.log(`angle: ${angle}, ${arm.style.transform}`)
+
             prevAngle=angle
             angle=prevAngle+6;
             
@@ -69,32 +94,33 @@ play.addEventListener('click',function(req, res){
             else{
                 min=minCount
             }
-            console.log(`SECOND: ${sec}, MINUTE: ${min}`)
+
+            //display minutes and seconds in html/css
             minute.innerHTML=`<div class="font">Min</div><div class="font">${min}</div>`
             second.innerHTML=`<div class="font">Sec</div><div class="font">${sec}</div>`
         },1000)
 
     }
+    //logic to stop the stopwatch
     else if(playButtonStatus=='pause'){
-        play.innerHTML=
-        `<i class="fa-solid fa-play"></i>
-        <div class="cloud">Play</div>`
+        play.innerHTML=`<i class="fa-solid fa-play"></i>`
+        playCloud.textContent='Start'
+
         playButtonStatus='play'
-        console.log('Stop clicked', id)
-        console.log(`secCount: ${secCount}, minCount: ${minCount}`)
+
+        //this logic is useful when we stop the watch and need to resume if afterwards
         prevMinCount=minCount;
         prevSecCount=secCount;
         clearInterval(id);
-        console.log(`Stopped: ${prevMinCount}, ${prevSecCount}`)
     }
-    
 })
 
+//to reset the secCount and minCount when reset button clicked
 reset.addEventListener('click',function(req, res){
-    console.log('Reset clicked')
+
     clearInterval(id);
     id=undefined
-    console.log(`ID: ${id}`);
+
     prevMinCount=0;
     prevSecCount=0;
     minCount=0;
